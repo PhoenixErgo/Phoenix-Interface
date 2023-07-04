54b364d359604633741cb2d4b372b636c4b6ba1e
star-works <farukhkhan1115@gmail.com>
2023-07-04 23:22:28 +0530

    first commit

---
+++ components/Main.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import HeaderCards from "./HeaderCards";
+import { Logo } from "./Logo";
+import MintingHodlERG from "./MintingHodlERG";
+import BurningHoldERG from "./BurningHoldERG";
+
+const Main = () => {
+  return (
+    <>
+      <div className="flex container items-center justify-between mx-auto px-3 lg:px-5 py-4">
+        <Logo />
+        <button
+          type="button"
+          className="focus:outline-none text-white primary-gradient hover:opacity-80 focus:ring-4 focus:ring-purple-300 font-medium rounded text-md px-3 sm:px-5 py-2 sm:py-2.5"
+        >
+          CONNECT WALLET
+        </button>
+      </div>
+      <div className="primary-gradient w-full py-3 text-center">
+        <p className="border-bottom text-white font-medium text-lg uppercase relative after:absolute after:-bottom-[11px] after:left-1/2 after:-translate-x-1/2 after:w-[130%] after:bg-white after:h-1 inline-block">
+          HODLERG 3%
+        </p>
+      </div>
+      <div className="container mx-auto px-3 lg:px-5 my-10 sm:flex items-center justify-between space-y-4 sm:space-x-4 xl:space-x-10">
+        <HeaderCards title="Current price" text="0.000 ERG" />
+        <HeaderCards title="Circulating supply" text="97,090,373,01 holdERG" />
+        <HeaderCards title="TVL" text="1 ERG" />
+      </div>
+      <div className="lg:flex items-center px-3 my-10 lg:my-20">
+        <MintingHodlERG />
+        <BurningHoldERG />
+      </div>
+    </>
+  );
+};
+
+export default Main;
---
+++ components/MintingHodlERG.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const MintingHodlERG = () => {
+  return (
+    <>
+      <div className="max-w-md mx-auto mb-10 lg:mb-0">
+        <h4 className="text-black text-xl font-medium">Minting hodlERG</h4>
+        <p className="text-black my-3">
+          Mint hodlERG with no fees. You have the freedom to mint as much as you
+          desire at the current price. it's important to note that the minting
+          process does not directly affect the token's pricing dynamics.
+        </p>
+
+        <div className="flex bg-gray-200 shadow-lg justify-between rounded-md items-start h-full">
+          <div className="flex flex-col w-full h-full">
+            <input
+              className="w-full border-b-2 border-l-0 border-r-0 border-t-0 border-gray-300 bg-transparent text-gray-500 font-medium text-md h-14 focus:outline-none focus:ring-0 focus:border-primary focus-within:outline-none focus-within:shadow-none focus:shadow-none pl-4"
+              placeholder="Amount"
+              type="text"
+            />
+            <span className="text-black font-medium text-md pl-4 mt-2">
+              0 ERG
+            </span>
+          </div>
+
+          <button className="h-24 whitespace-nowrap focus:outline-none text-white primary-gradient hover:opacity-80 focus:ring-4 focus:ring-purple-300  focus:shadow-none font-medium rounded text-md px-5 py-2.5">
+            MINT HODLERG
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MintingHodlERG;
