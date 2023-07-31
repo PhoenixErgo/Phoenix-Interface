dfc10d6a4f8fab2d1198d073081240880b20cd93
Ansar Ali <ansar@amharctech.com>
2023-07-31 22:34:26 +0600

    wallet and ui fix done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return( <div>
+     <Component {...pageProps} />
+  </div>)
 }
