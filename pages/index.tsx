072e9f1a16cba11f0f321aa1c8e0edb13f732fd2
star-works <farukhkhan1115@gmail.com>
2023-07-31 12:25:19 +0530

    toast issue fix import toast container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import Main from "@/components/Main";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const notify = () => toast("Wow so easy!");
+
   return (
     <>
       <Head>
         <title>Phoenix</title>
       </Head>
+      <ToastContainer />
       <main className={`bg-[#f5f5f5] min-h-screen ${inter.className}`}>
         <Main />
       </main>
