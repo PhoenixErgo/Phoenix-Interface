e011a8586cb3426c54a58dde6d28d36d7c1780ac
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-08-16 01:17:42 -0400

    fix css bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './Components/**/*.{js,ts,jsx,tsx}',
-    './Components/**/**/*.{js,ts,jsx,tsx}',
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ee4d01",
+      },
+    },
   },
   plugins: [],
 };
