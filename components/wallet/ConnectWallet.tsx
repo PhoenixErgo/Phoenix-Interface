eebce38039d643efd824d8789b92a56a1b6b8155
star-works <farukhkhan1115@gmail.com>
2023-08-22 13:05:28 +0530

    tool tip added

--- components/wallet/ConnectWallet.tsx
+++ components/wallet/ConnectWallet.tsx
@@ -748,10 +748,29 @@ const ConnectWallet: React.FC<IProps> = (props) => {
               </div>
             </div>
 
-            <div>
-              <p className="mb-2 mt-4 text-[14px] font-semibold text-darkblack font-VelaSanSemiBold">
+            <div className="flex items-center mb-2 mt-4">
+              <p className=" text-[14px] font-semibold text-darkblack font-VelaSanSemiBold">
                 Active address
               </p>
+              <Tooltip
+                placement="top"
+                title="Check the Address."
+                className={commonStyle.addressIcons}
+              >
+                <svg
+                  className="ml-1 cursor-pointer"
+                  viewBox="64 64 896 896"
+                  focusable="false"
+                  data-icon="info-circle"
+                  width="1em"
+                  height="1em"
+                  fill="#BFBFBF"
+                  aria-hidden="true"
+                >
+                  <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
+                  <path d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z"></path>
+                </svg>
+              </Tooltip>
             </div>
             <div
               className={`flex align-items-center justify-between ${commonStyle.ergoBalanceDiv}`}
