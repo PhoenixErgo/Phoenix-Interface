3d38b9bd50058c23159f6b2206e6f25cf709a108
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-10-13 20:17:28 -0400

    display only tokens in wallet

--- blockchain/ergo/ergopay/walletUtils.ts
+++ blockchain/ergo/ergopay/walletUtils.ts
@@ -1,3 +1,4 @@
+import {OutputInfo, TransactionInfo} from "@/blockchain/ergo/explorerApi";
 
 export async function getTotalBalance(explorerClient: any, address: string): Promise<any[]>{
   const convertedResponse: any[] = [];
@@ -11,12 +12,28 @@ export async function getTotalBalance(explorerClient: any, address: string): Pro
   convertedResponse.push(ergBalance);
 
   for (const token of balance.tokens) {
-    const tokenBalance = {
-      tokenId: token.tokenId,
-      balance: token.amount.toString(),
-    };
-    convertedResponse.push(tokenBalance);
+    const tokenIssuanceBox = await getTokenIssuanceBox(explorerClient, token.tokenId);
+    if(isTokenAsset(tokenIssuanceBox)){
+      const tokenBalance = {
+        tokenId: token.tokenId,
+        balance: token.amount.toString(),
+      };
+      convertedResponse.push(tokenBalance);
+    }
   }
 
   return convertedResponse;
+}
+
+async function getTokenIssuanceBox(explorerClient: any, tokenId: string): Promise<OutputInfo> {
+  const tokenIssuerBox: OutputInfo = (await explorerClient.getApiV1BoxesP1(tokenId)).data;
+  const spentTransactionId: string = tokenIssuerBox.spentTransactionId!;
+
+  const mintTx: TransactionInfo = (await explorerClient.getApiV1TransactionsP1(spentTransactionId)).data;
+
+  return mintTx.outputs!.find((o) => o.assets?.find((a) => a.tokenId === tokenId))!;
+}
+
+function isTokenAsset(issuanceBox: OutputInfo): boolean {
+  return issuanceBox.additionalRegisters.R7 === undefined;
 }
