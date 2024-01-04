699229dfcb1c79c4d4922f2d87cb76747070f09f
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-01-03 22:50:15 -0500

    refactor

--- blockchain/ergo/phoenixContracts/BankContracts/HodlBankContract.ts
+++ blockchain/ergo/phoenixContracts/BankContracts/HodlBankContract.ts
@@ -8,13 +8,13 @@ interface BurnAmount {
 }
 
 export class HodlBankContract {
-    private readonly contractBox: OutputInfo;
+    protected readonly contractBox: OutputInfo;
 
     constructor(box: OutputInfo) {
         this.contractBox = box;
     }
 
-    getTotalTokenSupply(): bigint {
+    public getTotalTokenSupply(): bigint {
         const r4 = this.contractBox.additionalRegisters.R4;
         if (!r4) {
             throw new Error("R4 not found");
@@ -22,7 +22,7 @@ export class HodlBankContract {
         return BigInt(r4.renderedValue);
     }
 
-    getPrecisionFactor(): bigint {
+    public getPrecisionFactor(): bigint {
         const r5 = this.contractBox.additionalRegisters.R5;
         if (!r5) {
             throw new Error("R5 not found");
@@ -30,7 +30,7 @@ export class HodlBankContract {
         return BigInt(r5.renderedValue);
     }
 
-    getMinBankValue(): bigint {
+    public getMinBankValue(): bigint {
         const r6 = this.contractBox.additionalRegisters.R6;
         if (!r6) {
             throw new Error("R6 not found");
@@ -38,7 +38,7 @@ export class HodlBankContract {
         return BigInt(r6.renderedValue);
     }
 
-    getDevFeeNum(): bigint {
+    public getDevFeeNum(): bigint {
         const r7 = this.contractBox.additionalRegisters.R7;
         if (!r7) {
             throw new Error("R7 not found");
@@ -46,7 +46,7 @@ export class HodlBankContract {
         return BigInt(r7.renderedValue);
     }
 
-    getBankFeeNum(): bigint {
+    public getBankFeeNum(): bigint {
         const r8 = this.contractBox.additionalRegisters.R8;
         if (!r8) {
             throw new Error("R8 not found");
@@ -54,27 +54,27 @@ export class HodlBankContract {
         return BigInt(r8.renderedValue);
     }
 
-    getTVL(): bigint {
+    public getTVL(): bigint {
         return BigInt(this.contractBox.value);
     }
 
-    getHodlERGReserveAmount(): bigint {
+    public getHodlReserveAmount(): bigint {
         return BigInt(this.contractBox.assets![1].amount);
     }
 
-    getBoxBlockHeight(): number {
+    public getBoxBlockHeight(): number {
         return this.contractBox.settlementHeight;
     }
 
-    getHodlERG3EmissionAmount(): bigint {
-        return this.getTotalTokenSupply() - this.getHodlERGReserveAmount();
+    public getHodlEmissionAmount(): bigint {
+        return this.getTotalTokenSupply() - this.getHodlReserveAmount();
     }
 
-    getProtocolFeesCollected(): bigint {
-        return this.getTVL() -  this.getHodlERG3EmissionAmount();
+    public getProtocolFeesCollected(): bigint {
+        return this.getTVL() -  this.getHodlEmissionAmount();
     }
 
-    getHodlERGPrice(): bigint {
+    public getHodlPrice(): bigint {
         const reserveIn = BigInt(this.contractBox.value);
         const totalTokenSupply = this.getTotalTokenSupply();
         const hodlCoinsIn = BigInt(this.contractBox.assets![1].amount);
@@ -84,20 +84,20 @@ export class HodlBankContract {
         return (reserveIn * precisionFactor) / hodlCoinsCircIn;
     }
 
-    mintAmount(hodlMintAmt: bigint): bigint {
-        const price = this.getHodlERGPrice();
+    public mintAmount(hodlMintAmt: bigint): bigint {
+        const price = this.getHodlPrice();
         const precisionFactor = this.getPrecisionFactor();
 
         return (hodlMintAmt * price) / precisionFactor;
     }
 
-    burnAmount(hodlBurnAmt: bigint): BurnAmount {
+    public burnAmount(hodlBurnAmt: bigint): BurnAmount {
         const feeDenom = BigInt(1000);
 
         const devFee = this.getDevFeeNum();
         const bankFee = this.getBankFeeNum();
 
-        const price = this.getHodlERGPrice();
+        const price = this.getHodlPrice();
         const precisionFactor = this.getPrecisionFactor();
 
         const beforeFees = (hodlBurnAmt * price) / precisionFactor;
@@ -108,9 +108,9 @@ export class HodlBankContract {
         const expectedAmountWithdrawn = beforeFees - bankFeeAmount - devFeeAmount;
 
         return {
-            expectedAmountWithdrawn: expectedAmountWithdrawn, // amount of ERG returned to user
-            devFeeAmount: devFeeAmount,
-            bankFeeAmount: bankFeeAmount,
+            expectedAmountWithdrawn, // amount of ERG returned to user
+            devFeeAmount,
+            bankFeeAmount,
         };
     }
 }
