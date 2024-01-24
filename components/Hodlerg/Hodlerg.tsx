85f3582a0679f682ebca23e7f83386f716b2a4a5
Anastasia Polozova <polozova.anastasi@gmail.com>
2024-01-24 20:29:59 -0300

    fix second column

--- components/Hodlerg/Hodlerg.tsx
+++ components/Hodlerg/Hodlerg.tsx
@@ -16,22 +16,23 @@ const Hodlerg = (props: IProps) => {
     <>
       <div className="min-h-[70vh] flex flex-col  md:mx-20 mx-5">
         <div className="text-black font-bold text-3xl my-5 lg:mb-4 text-center">HODLERG</div>
-        <div className="mt-8">
-          <HeaderCards
-            title="Current price"
-            text={`${ergdata.currentPrice} ERG`}
-          />
-          <HeaderCards
-            title="Emission amount"
-            text={`${ergdata.circulatingSupply} hodlERG`}
-          />
-          <HeaderCards
-            title="TVL"
-            text={`${ergdata.tvl} ERG`}
-          />
-        </div>
+
         <div className="flex-col md:flex-row flex">
           <div className="w-full flex flex-col items-center px-2 sm:px-3 my-10  lg:my-20">
+            <div className="mt-8">
+              <HeaderCards
+                title="Current price"
+                text={`${ergdata.currentPrice} ERG`}
+              />
+              <HeaderCards
+                title="Emission amount"
+                text={`${ergdata.circulatingSupply} hodlERG`}
+              />
+              <HeaderCards
+                title="TVL"
+                text={`${ergdata.tvl} ERG`}
+              />
+            </div>
             <h2 className="text-black font-bold text-3xl mb-5 lg:mb-8">HODLERG 3%</h2>
             <p className="text-black my-3 min-h-[100px] flex items-end">
               Mint hodlERG with no fees. You have the freedom to mint as much as you
@@ -48,6 +49,20 @@ const Hodlerg = (props: IProps) => {
           </div>
 
           <div className="w-full flex flex-col items-center px-2 sm:px-3 my-10 lg:my-20">
+            <div className="mt-8 w-full">
+              <HeaderCards
+                title="Current price"
+                text={`${ergdata.currentPrice} ERG`}
+              />
+              <HeaderCards
+                title="Emission amount"
+                text={`${ergdata.circulatingSupply} hodlERG`}
+              />
+              <HeaderCards
+                title="TVL"
+                text={`${ergdata.tvl} ERG`}
+              />
+            </div>
             <h2 className="text-black font-bold text-3xl mb-5 lg:mb-8">HODLERG 10%</h2>
             <p className="text-black my-3 min-h-[100px] flex items-end">
               Mint hodlERG with no fees. You have the freedom to mint as much as you
