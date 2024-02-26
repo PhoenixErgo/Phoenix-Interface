cb30b537e3290d2f4f17fc17aaf5604865649784
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-02-26 16:52:11 -0500

    vercel analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Main from '../components/Main';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
